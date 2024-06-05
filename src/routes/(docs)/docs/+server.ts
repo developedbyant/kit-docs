a994414120577b974f2e5a973c0d097b6f083ea4
developedByant <developedbyant@gmail.com>
2024-06-04 22:35:08 -0400

    bug fixes

--- src/routes/(docs)/docs/+server.ts
+++ src/routes/(docs)/docs/+server.ts
@@ -1,5 +1,5 @@
 import { json } from "@sveltejs/kit";
-                import { cookieSet } from "kitdocs/utils";
+                import { cookieSet } from "$lib/utils.js";
                 import type { DocTheme } from "kitdocs"
                 import type { RequestHandler } from "./$types.js";
                 
